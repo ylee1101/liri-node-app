c651d2ea00ad74cd353de80e4eb3b9ba9161ecf5
ylee1101 <yongho19911101@hotmail.com>
2017-08-23 00:33:39 -0700

    tested and done

--- liri.js
+++ liri.js
@@ -3,7 +3,7 @@
 var fs = require('fs');
 var keys = require('./keys.js') 
 var twitter = require('twitter');
-var spotify = require('spotify');
+var Spotify = require('node-spotify-api');
 var request = require('request');
 var liriArg = process.argv[2];
 
@@ -23,7 +23,7 @@ switch(liriArg) {
 		doWhatItSays();
 		break;
 	default: 
-		console.log("What are you trying to do? Try again with my-tweets or spotify-this-song or movie-this or do-what-it-says.");
+		console.log("What are you trying to do? Try again with: "  +"\r\n"+ "my-tweets" +"\r\n"+ "spotify-this-song" +"\r\n"+ "movie-this" +"\r\n"+ "do-what-it-says.");
 
 };
 
@@ -71,6 +71,11 @@ function myTweets() {
 // spotify section
 
 function spotifyThisSong(spotifySong) {
+	var spotify = new Spotify({
+  		id: keys.spotifyKey.client_id,
+  		secret: keys.spotifyKey.client_secret
+	});
+
 	var spotifySong = process.argv[3];
 	
 	if(!spotifySong) {
@@ -85,15 +90,16 @@ function spotifyThisSong(spotifySong) {
         	return;
     	}
     	else {
+    		// console.log(data);
     		var songInfo = data.tracks.items;
-    		
+
     		for (var i = 0; i < 5; i++) {
     			if (songInfo[i]!= undefined) {
     				var spotifyResults = 
     				"Artist: " + songInfo[i].artists[0].name + "\r\n" + 
     				"Song: " + songInfo[i].name + "\r\n" +
     				"Album: " + songInfo[i].album.name + "\r\n" +
-    				"-------------" + i + "---------" + "\r\n";
+    				"------------------------------ " + i + " ------------------------------" + "\r\n";
     				console.log(spotifyResults);
     				log(spotifyResults);
 
@@ -106,6 +112,7 @@ function spotifyThisSong(spotifySong) {
 // // // omdb section 
 
 function movieThis(){
+
 		var movie = process.argv[3];
 			if(!movie){
 				movie = 'Mr. Nobody.';
@@ -113,12 +120,12 @@ function movieThis(){
 		
 		params = movie
 
-		request("http://www.omdbapi.com/?t=" + params + "&y=&plot=short&r=json&tomatoes=true", function (error, response, body) {
+		request("http://www.omdbapi.com/?apikey=" + keys.omdbKey.omdb_key + "&t=" + params + "&y=&plot=short&r=json&tomatoes=true", function (error, response, body) {
 			if (!error && response.statusCode == 200) {
 				var movieObject = JSON.parse(body);
-				console.log(movieObject); // Show the text in the terminal
+				// console.log(movieObject); 
 				var movieResults =
-				"------------------------------ begin ------------------------------" + "\r\n"
+				"------------------------------ begin ------------------------------" + "\r\n" +
 				"Title: " + movieObject.Title+"\r\n"+
 				"Year: " + movieObject.Year+"\r\n"+
 				"Imdb Rating: " + movieObject.imdbRating+"\r\n"+
@@ -130,7 +137,8 @@ function movieThis(){
 				"Rotten Tomatoes URL: " + movieObject.tomatoURL + "\r\n" + 
 				"------------------------------ finish ------------------------------" + "\r\n";
 				console.log(movieResults);
-				log(movieResults); // calling log function
+
+				log(movieResults); 
 			} 
 			else {
 				console.log("Error :"+ error);
@@ -161,29 +169,3 @@ function log(logResults) {
     }
   });
 }
-
-
-// node liri.js spotify-this-song '<song name here>'
-// This will show the following information about the song in your terminal/bash window
-// Artist(s)
-// The song's name
-// A preview link of the song from Spotify
-// The album that the song is from
-// If no song is provided then your program will default to "The Sign" by Ace of Base.
-// You will utilize the node-spotify-api package in order to retrieve song information from the Spotify API.
-// Like the Twitter API, the Spotify API requires you sign up as a developer to generate the necessary credentials. You can follow these steps in order to generate a client id and client secret:
-// Step One: Visit https://developer.spotify.com/my-applications/#!/
-// Step Two: Either login to your existing Spotify account or create a new one (a free account is fine) and log in.
-// Step Three: Once logged in, navigate to https://developer.spotify.com/my-applications/#!/applications/create to register a new application to be used with the Spotify API. You can fill in whatever you'd like for these fields. When finished, click the "complete" button.
-// Step Four: On the next screen, scroll down to where you see your client id and client secret. Copy these values down somewhere, you'll need them to use the Spotify API and the node-spotify-api package. See the
-// node liri.js movie-this '<movie name here>'
-// This will output the following information to your terminal/bash window:
-//    * Title of the movie.
-//    * Year the movie came out.
-//    * IMDB Rating of the movie.
-//    * Rotten Tomatoes Rating of the movie.
-//    * Country where the movie was produced.
-//    * Language of the movie.
-//    * Plot of the movie.
-//    * Actors in the movie.
-
